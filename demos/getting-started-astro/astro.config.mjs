fab254f72a06700c01dffec75a1f1aa66d338e76
Rodney Lab <codesign@rodneylab.com>
2022-03-28 13:14:51 +0100

    fix(demos/getting-started-astro): 💫 update for Astro breaking changes

--- demos/getting-started-astro/astro.config.mjs
+++ demos/getting-started-astro/astro.config.mjs
@@ -1,5 +1,8 @@
 import { defineConfig } from 'astro/config';
 
+import react from '@astrojs/react';
+import svelte from '@astrojs/svelte';
+
 // https://astro.build/config
 export default defineConfig({
   buildOptions: {
@@ -7,7 +10,7 @@ export default defineConfig({
     site: 'https://astro.build/',
   },
   // Comment out "renderers: []" to enable Astro's default component support.
-  renderers: ['@astrojs/renderer-react', '@astrojs/renderer-svelte'],
+  integrations: [react(), svelte()],
   vite: {
     define: {
       'process.env.VITE_BUILD_TIME': JSON.stringify(new Date().toISOString()),
