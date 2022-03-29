658b007ef0f2b40afb729aa34a81883f60637759
Rodney Lab <codesign@rodneylab.com>
2022-03-29 06:53:25 +0100

    fix(demos/astro-contact-form): 💫 switch to JavaScript functions

---
+++ demos/astro-contact-form/netlify/functions/contact-form-message.js
@@ -0,0 +1,80 @@
+import axios from "axios";
+
+const { TELEGRAM_BOT_API_TOKEN, TELEGRAM_BOT_CHAT_ID } = process.env;
+
+async function notifyViaTelegramBot({
+  honeyBotFlaggedSpam,
+  name,
+  email,
+  message,
+}) {
+  try {
+    const data = JSON.stringify(
+      {
+        honeyBotFlaggedSpam,
+        name,
+        email,
+        message,
+      },
+      null,
+      2
+    );
+    const text = `Comment: ${data}`;
+    await axios({
+      url: `https://api.telegram.org/bot${TELEGRAM_BOT_API_TOKEN}/sendMessage`,
+      method: "POST",
+      data: {
+        chat_id: TELEGRAM_BOT_CHAT_ID,
+        text,
+      },
+    });
+    return { successful: true };
+  } catch (error) {
+    let message;
+    if (error.response) {
+      message = `Telegram server responded with non 2xx code: ${error.response.data}`;
+    } else if (error.request) {
+      message = `No Telegram response received: ${error.request}`;
+    } else {
+      message = `Error setting up telegram response: ${error.message}`;
+    }
+    return { successful: false, error: message };
+  }
+}
+
+export async function handler({ body, httpMethod }) {
+  try {
+    if (httpMethod !== "POST") {
+      return {
+        statusCode: 405,
+        body: "Method Not Allowed",
+      };
+    }
+
+    const data = JSON.parse(body);
+    const { botField, email, name, message } = data;
+
+    const { error: telegramError } = await notifyViaTelegramBot({
+      honeyBotFlaggedSpam: botField !== "",
+      email,
+      name,
+      message,
+    });
+
+    if (telegramError) {
+      return {
+        statusCode: 400,
+        body: telegramError,
+      };
+    }
+
+    return { statusCode: 200, body: "Over and out." };
+  } catch (error) {
+    return {
+      statusCode: 400,
+      body: `Handler error: ${error}`,
+    };
+  }
+}
+
+export default handler;
--- demos/astro-contact-form/src/components/ContactForm.svelte
+++ demos/astro-contact-form/src/components/ContactForm.svelte
@@ -26,7 +26,7 @@
           "Content-Type": "application/json",
         },
         body: JSON.stringify({
-          bot_field: botField,
+          botField,
           email,
           name,
           message,
