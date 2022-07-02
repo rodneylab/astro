04e9bdd70b9ce79c12a383a0b448065c7d0cde49
Rodney Lab <codesign@rodneylab.com>
2022-07-02 06:45:58 +0100

    style(demos/getting-started-astro): 🛁  switch to tab indentation

--- demos/getting-started-astro/astro.config.mjs
+++ demos/getting-started-astro/astro.config.mjs
@@ -5,13 +5,13 @@ import svelte from '@astrojs/svelte';
 
 // https://astro.build/config
 export default defineConfig({
-  sitemap: true,
-  site: 'https://example.com/',
-  integrations: [react(), svelte()],
-  vite: {
-    define: {
-      'process.env.VITE_BUILD_TIME': JSON.stringify(new Date().toISOString()),
-    },
-    plugins: [],
-  },
+	sitemap: true,
+	site: 'https://example.com/',
+	integrations: [react(), svelte()],
+	vite: {
+		define: {
+			'process.env.VITE_BUILD_TIME': JSON.stringify(new Date().toISOString()),
+		},
+		plugins: [],
+	},
 });
