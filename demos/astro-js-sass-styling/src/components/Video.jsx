f74169963b02b7027f6c0802aa4366dec085cbcc
Rodney Lab <codesign@rodneylab.com>
2022-08-04 08:16:05 +0100

    style(demos/astro-js-sass-styling): 🛁  switch to tab indentation

--- demos/astro-js-sass-styling/src/components/Video.jsx
+++ demos/astro-js-sass-styling/src/components/Video.jsx
@@ -2,32 +2,32 @@ import styles from '$components/Video.module.scss';
 import { useState } from 'react';
 
 export const ReactExample = function ReactExample() {
-  const [altColours, setAltColours] = useState(false);
+	const [altColours, setAltColours] = useState(false);
 
-  return (
-    <section className={`${styles.container}${altColours ? ` ${styles['container-alt']}` : ''}`}>
-      <h2>Example React Component</h2>
-      <div className="video-container">
-        <iframe
-          width="560"
-          height="315"
-          src="https://www.youtube-nocookie.com/embed/PJ0QSJpJn2U"
-          title="Should you Stop Using React"
-          frameBorder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowFullScreen
-        />
-      </div>
-      <button
-        className={`${styles.button}${altColours ? ` ${styles['button-alt']}` : ''}`}
-        onClick={() => {
-          setAltColours(!altColours);
-        }}
-      >
-        <span className="screen-reader-text">Toggle colours</span>
-      </button>
-    </section>
-  );
+	return (
+		<section className={`${styles.container}${altColours ? ` ${styles['container-alt']}` : ''}`}>
+			<h2>Example React Component</h2>
+			<div className="video-container">
+				<iframe
+					width="560"
+					height="315"
+					src="https://www.youtube-nocookie.com/embed/PJ0QSJpJn2U"
+					title="Should you Stop Using React"
+					frameBorder="0"
+					allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+					allowFullScreen
+				/>
+			</div>
+			<button
+				className={`${styles.button}${altColours ? ` ${styles['button-alt']}` : ''}`}
+				onClick={() => {
+					setAltColours(!altColours);
+				}}
+			>
+				<span className="screen-reader-text">Toggle colours</span>
+			</button>
+		</section>
+	);
 };
 
 export default ReactExample;
--- demos/astro-js-sass-styling/src/components/Video.svelte
+++ demos/astro-js-sass-styling/src/components/Video.svelte
@@ -1,52 +1,52 @@
 <script>
-  $: altColours = false;
+	$: altColours = false;
 </script>
 
 <section class={`container${altColours ? ' container-alt' : ''}`}>
-  <h2>Svelte Component</h2>
-  <div class="video-container">
-    <iframe
-      title="Trying Svelte for the Third Time"
-      width="560"
-      height="315"
-      src="https://www.youtube-nocookie.com/embed/xgER1OutVvU"
-      frameborder="0"
-      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-      allowfullscreen
-    />
-  </div>
-  <button
-    class={`button${altColours ? ' button-alt' : ''}`}
-    on:click={() => {
-      altColours = !altColours;
-    }}><span class="screen-reader-text">Toggle colours</span></button
-  >
+	<h2>Svelte Component</h2>
+	<div class="video-container">
+		<iframe
+			title="Trying Svelte for the Third Time"
+			width="560"
+			height="315"
+			src="https://www.youtube-nocookie.com/embed/xgER1OutVvU"
+			frameborder="0"
+			allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+			allowfullscreen
+		/>
+	</div>
+	<button
+		class={`button${altColours ? ' button-alt' : ''}`}
+		on:click={() => {
+			altColours = !altColours;
+		}}><span class="screen-reader-text">Toggle colours</span></button
+	>
 </section>
 
 <style lang="scss">
-  @import '../styles/variables';
+	@import '../styles/variables';
 
-  .container {
-    display: flex;
-    flex-direction: column;
-    background: hsl($colour-brand-hue $colour-brand-saturation $colour-brand-luminance);
-    align-items: center;
-    width: 100%;
-    padding: $spacing-8 $spacing-0;
-    color: hsl($colour-dark-text-hue $colour-dark-text-saturation $colour-dark-text-luminance);
-  }
+	.container {
+		display: flex;
+		flex-direction: column;
+		background: hsl($colour-brand-hue $colour-brand-saturation $colour-brand-luminance);
+		align-items: center;
+		width: 100%;
+		padding: $spacing-8 $spacing-0;
+		color: hsl($colour-dark-text-hue $colour-dark-text-saturation $colour-dark-text-luminance);
+	}
 
-  .container-alt {
-    background: hsl($colour-secondary-hue $colour-secondary-saturation $colour-secondary-luminance);
+	.container-alt {
+		background: hsl($colour-secondary-hue $colour-secondary-saturation $colour-secondary-luminance);
 
-    color: hsl($colour-light-text-hue $colour-light-text-saturation $colour-light-text-luminance);
-  }
+		color: hsl($colour-light-text-hue $colour-light-text-saturation $colour-light-text-luminance);
+	}
 
-  .button {
-    background: hsl($colour-secondary-hue $colour-secondary-saturation $colour-secondary-luminance);
-  }
+	.button {
+		background: hsl($colour-secondary-hue $colour-secondary-saturation $colour-secondary-luminance);
+	}
 
-  .button-alt {
-    background: hsl($colour-brand-hue $colour-brand-saturation $colour-brand-luminance);
-  }
+	.button-alt {
+		background: hsl($colour-brand-hue $colour-brand-saturation $colour-brand-luminance);
+	}
 </style>
