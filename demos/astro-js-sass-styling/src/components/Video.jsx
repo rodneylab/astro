ae49001058811b821d9865b7ddeab7dfd756ab80
Rodney Lab <codesign@rodneylab.com>
2022-09-16 09:01:31 +0100

    fix(demos/astro-js-sass-styling): 💫 update import aliases

--- demos/astro-js-sass-styling/src/components/Video.jsx
+++ demos/astro-js-sass-styling/src/components/Video.jsx
@@ -1,4 +1,4 @@
-import styles from '$components/Video.module.scss';
+import styles from '~components/Video.module.scss';
 import { useState } from 'react';
 
 export const ReactExample = function ReactExample() {
