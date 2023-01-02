fb52fbc8b738845282525eae2660b7d7a16072e0
Rodney Lab <codesign@rodneylab.com>
2023-01-02 07:58:13 +0000

    fix(demos/astro-vanilla-extract): 💫 update Astro config

--- demos/astro-vanilla-extract/astro.config.mjs
+++ demos/astro-vanilla-extract/astro.config.mjs
@@ -8,6 +8,7 @@ import svelte from '@astrojs/svelte';
 
 // https://astro.build/config
 export default defineConfig({
+	server: { port: 3001 },
 	vite: {
 		plugins: [imagetools(), vanillaExtractPlugin()],
 	},
