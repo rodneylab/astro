84e11e4265c8ec7e4d7afc7fe95d705ab44c4e5f
Rodney Lab <codesign@rodneylab.com>
2023-01-12 06:10:18 +0000

    fix(demos/astro-contact-form): 💫 update Astro config

--- demos/astro-contact-form/astro.config.mjs
+++ demos/astro-contact-form/astro.config.mjs
@@ -4,5 +4,7 @@ import svelte from '@astrojs/svelte';
 
 // https://astro.build/config
 export default defineConfig({
+	preview: { port: 3001 },
+	server: { port: 3001 },
 	integrations: [svelte()],
 });
