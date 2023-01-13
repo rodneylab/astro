4bb8f84d79d7bbeb29eb0ffba8b6633107ed0216
Rodney Lab <codesign@rodneylab.com>
2023-01-13 18:13:03 +0000

    fix(demos/astro-js-404-page): 💫 update Astro config

--- demos/astro-js-404-page/astro.config.mjs
+++ demos/astro-js-404-page/astro.config.mjs
@@ -1,4 +1,4 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({ preview: { port: 3001 }, server: { port: 3001 } });
