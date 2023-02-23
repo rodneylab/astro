cb8914591a841da3b04a33234f96aaabf0ac7d28
Rodney Lab <codesign@rodneylab.com>
2023-02-23 06:19:17 +0000

    fix(demos/astro-self-hosted-fonts): 💫 update Astro config

--- demos/astro-self-hosted-fonts/astro.config.mjs
+++ demos/astro-self-hosted-fonts/astro.config.mjs
@@ -3,6 +3,8 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 	integrations: [],
+	preview: { port: 3001 },
+	server: { port: 3001 },
 	vite: {
 		ssr: {
 			external: ['svgo'],
