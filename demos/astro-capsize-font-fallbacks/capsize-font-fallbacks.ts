fa8c3d5ef75254fb95236aa2bf4df7e90391987d
Rodney Lab <codesign@rodneylab.com>
2023-05-18 08:56:31 +0100

    fix(demos/astro-capsize-font-fallbacks): 💫 update capsize script

--- demos/astro-capsize-font-fallbacks/capsize-font-fallbacks.ts
+++ demos/astro-capsize-font-fallbacks/capsize-font-fallbacks.ts
@@ -2,34 +2,20 @@ import { createFontStack } from '@capsizecss/core';
 import arial from '@capsizecss/metrics/arial';
 import openSans from '@capsizecss/metrics/openSans';
 import roboto from '@capsizecss/metrics/roboto';
-import kebabCase from 'just-kebab-case';
 import { writeFileSync } from 'node:fs';
 import path from 'node:path';
 
 const __dirname = path.resolve();
 const outputPath = path.join(__dirname, 'src/styles/font-fallbacks.css');
 
-const { fontFaces, fontFamily } = createFontStack([openSans, arial, roboto], {
-	fontFaceFormat: 'styleObject',
-});
+const { fontFaces, fontFamily } = createFontStack([openSans, arial, roboto]);
 
 const css = `/* This file is generated using capsize. Run \`vite-node capsize-font-fallbacks.ts\` to refresh. */
 
 :root {
   --font-family: ${fontFamily}, sans-serif;
 }
-${fontFaces
-	.map(
-		(element) => `
-@font-face {
-  font-family: '${element['@font-face'].fontFamily}';
-${Object.entries(element['@font-face'])
-	.filter(([key]) => key !== 'fontFamily')
-	.map(([key, value]) => `  ${kebabCase(key)}: ${value}`)
-	.join(';\n')}
-}`,
-	)
-	.join('\n')}
-`;
+
+${fontFaces}`;
 
 writeFileSync(outputPath, css);
