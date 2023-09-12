a8c9f442d34a2098f1d144fe340c8d691476d299
Rodney Lab <codesign@rodneylab.com>
2023-09-12 10:10:34 +0100

    fix(demos/astro-scroll-to-anchor): 💫 update packages
    ┌─────────────────────────────┬─────────┬─────────┬────────────────────────┐
    │ Package                     │ From    │ To      │ Dependents             │
    ├─────────────────────────────┼─────────┼─────────┼────────────────────────┤
    │ autoprefixer (dev)          │ 10.4.14 │ 10.4.15 │ astro-scroll-to-anchor │
    ├─────────────────────────────┼─────────┼─────────┼────────────────────────┤
    │ postcss (dev)               │ 8.4.27  │ 8.4.29  │ astro-scroll-to-anchor │
    ├─────────────────────────────┼─────────┼─────────┼────────────────────────┤
    │ prettier (dev)              │ 3.0.1   │ 3.0.3   │ astro-scroll-to-anchor │
    ├─────────────────────────────┼─────────┼─────────┼────────────────────────┤
    │ stylelint (dev)             │ 15.10.2 │ 15.10.3 │ astro-scroll-to-anchor │
    ├─────────────────────────────┼─────────┼─────────┼────────────────────────┤
    │ astro (dev)                 │ 2.10.1  │ 3.0.12  │ astro-scroll-to-anchor │
    ├─────────────────────────────┼─────────┼─────────┼────────────────────────┤
    │ prettier-plugin-astro (dev) │ 0.11.0  │ 0.12.0  │ astro-scroll-to-anchor │
    └─────────────────────────────┴─────────┴─────────┴────────────────────────┘

--- demos/astro-scroll-to-anchor/astro.config.mjs
+++ demos/astro-scroll-to-anchor/astro.config.mjs
@@ -2,8 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	preview: { port: 3001 },
-	server: { port: 3001 },
 	vite: {
 		ssr: {
 			external: ['svgo'],
