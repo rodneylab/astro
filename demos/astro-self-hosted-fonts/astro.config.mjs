fe26c0b5ba8b3b5e7145ba439d1d07bb66ee8f8d
Rodney Lab <codesign@rodneylab.com>
2023-09-14 09:01:20 +0100

    fix(demos/astro-self-hosted-fonts): 💫 update packages
    ┌─────────────────────────────┬─────────┬─────────┬─────────────────────────┐
    │ Package                     │ From    │ To      │ Dependents              │
    ├─────────────────────────────┼─────────┼─────────┼─────────────────────────┤
    │ autoprefixer (dev)          │ 10.4.14 │ 10.4.15 │ astro-self-hosted-fonts │
    ├─────────────────────────────┼─────────┼─────────┼─────────────────────────┤
    │ postcss (dev)               │ 8.4.27  │ 8.4.29  │ astro-self-hosted-fonts │
    ├─────────────────────────────┼─────────┼─────────┼─────────────────────────┤
    │ prettier (dev)              │ 3.0.1   │ 3.0.3   │ astro-self-hosted-fonts │
    ├─────────────────────────────┼─────────┼─────────┼─────────────────────────┤
    │ stylelint (dev)             │ 15.10.2 │ 15.10.3 │ astro-self-hosted-fonts │
    ├─────────────────────────────┼─────────┼─────────┼─────────────────────────┤
    │ astro (dev)                 │ 2.10.1  │ 3.0.13  │ astro-self-hosted-fonts │
    ├─────────────────────────────┼─────────┼─────────┼─────────────────────────┤
    │ prettier-plugin-astro (dev) │ 0.11.0  │ 0.12.0  │ astro-self-hosted-fonts │
    └─────────────────────────────┴─────────┴─────────┴─────────────────────────┘

--- demos/astro-self-hosted-fonts/astro.config.mjs
+++ demos/astro-self-hosted-fonts/astro.config.mjs
@@ -2,9 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [],
-	preview: { port: 3001 },
-	server: { port: 3001 },
 	vite: {
 		ssr: {
 			external: ['svgo'],
