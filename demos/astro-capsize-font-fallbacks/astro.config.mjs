0417bc05458ccfd8a75fdc2a6dce599bac9ed427
Rodney Lab <codesign@rodneylab.com>
2023-10-05 07:43:01 +0100

    fix(demos/astro-capsize-font-fallbacks): 💫 update packages
    ┌─────────────────┬─────────┬────────┬──────────────────────────────┐
    │ Package         │ From    │ To     │ Dependents                   │
    ├─────────────────┼─────────┼────────┼──────────────────────────────┤
    │ astro           │ 2.10.12 │ 3.2.2  │ astro-capsize-font-fallbacks │
    ├─────────────────┼─────────┼────────┼──────────────────────────────┤
    │ vite-node (dev) │ 0.34.2  │ 0.34.6 │ astro-capsize-font-fallbacks │
    └─────────────────┴─────────┴────────┴──────────────────────────────┘

--- demos/astro-capsize-font-fallbacks/astro.config.mjs
+++ demos/astro-capsize-font-fallbacks/astro.config.mjs
@@ -1,12 +1,4 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-	inlineStylesheets: 'auto',
-	preview: {
-		port: 3001,
-	},
-	server: {
-		port: 3001,
-	},
-});
+export default defineConfig({});
