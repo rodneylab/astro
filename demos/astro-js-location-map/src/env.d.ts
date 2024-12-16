a169fcc6b133ffad34b212e6bc3220d19f417023
Rodney Lab <codesign@rodneylab.com>
2024-12-16 09:04:37 +0000

    build(demos/astro-js-location-map): 🧱 update dependencies
    ┌───────────────────────┬─────────┬─────────┬───────────────────────┐
    │ Package               │ From    │ To      │ Dependents            │
    ├───────────────────────┼─────────┼─────────┼───────────────────────┤
    │ prettier (dev)        │ 3.3.3   │ 3.4.2   │ astro-js-location-map │
    ├───────────────────────┼─────────┼─────────┼───────────────────────┤
    │ stylelint (dev)       │ 16.11.0 │ 16.12.0 │ astro-js-location-map │
    ├───────────────────────┼─────────┼─────────┼───────────────────────┤
    │ typescript (dev)      │ 5.5.4   │ 5.7.2   │ astro-js-location-map │
    ├───────────────────────┼─────────┼─────────┼───────────────────────┤
    │ @astrojs/svelte (dev) │ 5.7.3   │ 7.0.1   │ astro-js-location-map │
    ├───────────────────────┼─────────┼─────────┼───────────────────────┤
    │ astro (dev)           │ 4.16.17 │ 5.0.5   │ astro-js-location-map │
    ├───────────────────────┼─────────┼─────────┼───────────────────────┤
    │ svelte (dev)          │ 4.2.19  │ 5.14.0  │ astro-js-location-map │
    ├───────────────────────┼─────────┼─────────┼───────────────────────┤
    │ svelte-check (dev)    │ 3.8.6   │ 4.1.1   │ astro-js-location-map │
    ├───────────────────────┼─────────┼─────────┼───────────────────────┤
    │ vite (dev)            │ 5.4.11  │ 6.0.3   │ astro-js-location-map │
    └───────────────────────┴─────────┴─────────┴───────────────────────┘

--- demos/astro-js-location-map/src/env.d.ts
+++ demos/astro-js-location-map/src/env.d.ts
@@ -2,9 +2,9 @@
 /// <reference types="astro/client" />
 
 interface ImportMetaEnv {
-	readonly PUBLIC_MAPBOX_ACCESS_TOKEN: string;
+  readonly PUBLIC_MAPBOX_ACCESS_TOKEN: string;
 }
 
 interface ImportMeta {
-	readonly env: ImportMetaEnv;
+  readonly env: ImportMetaEnv;
 }
--- demos/astro-js-location-map/src/shared/actions/map.ts
+++ demos/astro-js-location-map/src/shared/actions/map.ts
@@ -1,48 +1,55 @@
 export function setMap(
-	mapElement: HTMLElement,
-	{
-		latitude,
-		longitude,
-		zoom,
-		markerMarkup = '',
-	}: { latitude: number; longitude: number; zoom: number; markerMarkup?: string },
+  mapElement: HTMLElement,
+  {
+    latitude,
+    longitude,
+    zoom,
+    markerMarkup = "",
+  }: {
+    latitude: number;
+    longitude: number;
+    zoom: number;
+    markerMarkup?: string;
+  },
 ) {
-	(async () => {
-		const {
-			icon: leafletIcon,
-			map: leafletMap,
-			marker: leafletMarker,
-			tileLayer,
-		} = await import('leaflet');
+  (async () => {
+    const {
+      icon: leafletIcon,
+      map: leafletMap,
+      marker: leafletMarker,
+      tileLayer,
+    } = await import("leaflet");
 
-		const markerIcon = leafletIcon({
-			iconUrl: '/marker-icon.png',
-			iconSize: [25, 41],
-			iconAnchor: [10, 41],
-			popupAnchor: [2, -40],
-			iconRetinaUrl: '/marker-icon-2x.png',
-			shadowUrl: '/marker-shadow.png',
-		});
+    const markerIcon = leafletIcon({
+      iconUrl: "/marker-icon.png",
+      iconSize: [25, 41],
+      iconAnchor: [10, 41],
+      popupAnchor: [2, -40],
+      iconRetinaUrl: "/marker-icon-2x.png",
+      shadowUrl: "/marker-shadow.png",
+    });
 
-		const map = leafletMap(mapElement).setView([latitude, longitude], zoom);
-		tileLayer(
-			'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}{r}?access_token={accessToken}',
-			{
-				attribution:
-					'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-				maxZoom: 19,
-				id: 'mapbox/streets-v11',
-				tileSize: 512,
-				zoomOffset: -1,
-				accessToken: import.meta.env.PUBLIC_MAPBOX_ACCESS_TOKEN,
-				detectRetina: true,
-			},
-		).addTo(map);
+    const map = leafletMap(mapElement).setView([latitude, longitude], zoom);
+    tileLayer(
+      "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}{r}?access_token={accessToken}",
+      {
+        attribution:
+          'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 19,
+        id: "mapbox/streets-v11",
+        tileSize: 512,
+        zoomOffset: -1,
+        accessToken: import.meta.env.PUBLIC_MAPBOX_ACCESS_TOKEN,
+        detectRetina: true,
+      },
+    ).addTo(map);
 
-		if (markerMarkup !== '') {
-			leafletMarker([latitude, longitude], { icon: markerIcon }).bindPopup(markerMarkup).addTo(map);
-		} else {
-			leafletMarker([latitude, longitude], { icon: markerIcon }).addTo(map);
-		}
-	})();
+    if (markerMarkup !== "") {
+      leafletMarker([latitude, longitude], { icon: markerIcon })
+        .bindPopup(markerMarkup)
+        .addTo(map);
+    } else {
+      leafletMarker([latitude, longitude], { icon: markerIcon }).addTo(map);
+    }
+  })();
 }
