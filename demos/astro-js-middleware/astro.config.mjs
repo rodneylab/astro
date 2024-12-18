3f8049ca9dd1cad6751f16946bd8baffb66188d4
Rodney Lab <codesign@rodneylab.com>
2024-12-18 16:04:29 +0000

    build(demos/astro-js-middleware): 🧱 update dependencies
    ┌─────────────────────┬─────────┬─────────┬─────────────────────┐
    │ Package             │ From    │ To      │ Dependents          │
    ├─────────────────────┼─────────┼─────────┼─────────────────────┤
    │ stylelint (dev)     │ 16.11.0 │ 16.12.0 │ astro-js-middleware │
    ├─────────────────────┼─────────┼─────────┼─────────────────────┤
    │ typescript (dev)    │ 5.5.4   │ 5.7.2   │ astro-js-middleware │
    ├─────────────────────┼─────────┼─────────┼─────────────────────┤
    │ @astrojs/cloudflare │ 11.2.0  │ 12.1.0  │ astro-js-middleware │
    ├─────────────────────┼─────────┼─────────┼─────────────────────┤
    │ astro               │ 4.16.17 │ 5.0.9   │ astro-js-middleware │
    └─────────────────────┴─────────┴─────────┴─────────────────────┘

--- demos/astro-js-middleware/astro.config.mjs
+++ demos/astro-js-middleware/astro.config.mjs
@@ -1,9 +1,9 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import cloudflare from '@astrojs/cloudflare';
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-	output: 'server',
-	adapter: cloudflare(),
+  output: "server",
+  adapter: cloudflare(),
 });
--- demos/astro-js-middleware/src/middleware.ts
+++ demos/astro-js-middleware/src/middleware.ts
@@ -1,34 +1,34 @@
-import { defineMiddleware } from 'astro:middleware';
-import { pack } from 'msgpackr';
+import { defineMiddleware } from "astro:middleware";
+import { pack } from "msgpackr";
 
 const logtailSourceToken = import.meta.env.LOGTAIL_SOURCE_TOKEN;
 
 export const onRequest = defineMiddleware(async ({ locals, request }, next) => {
-	const { url } = request;
-	const { pathname } = new URL(url);
-	const timestamp = new Date().toISOString();
+  const { url } = request;
+  const { pathname } = new URL(url);
+  const timestamp = new Date().toISOString();
 
-	(locals as { timestamp: string }).timestamp = timestamp;
+  (locals as { timestamp: string }).timestamp = timestamp;
 
-	const logData = {
-		dt: timestamp,
-		level: 'info',
-		message: 'astro-middleware-log',
-		item: {
-			pathname,
-		},
-	};
+  const logData = {
+    dt: timestamp,
+    level: "info",
+    message: "astro-middleware-log",
+    item: {
+      pathname,
+    },
+  };
 
-	await fetch('https://in.logs.betterstack.com', {
-		method: 'POST',
-		headers: {
-			Authorization: `Bearer ${logtailSourceToken}`,
-			'Content-Type': 'application/msgpack',
-		},
-		body: pack(logData),
-	});
+  await fetch("https://in.logs.betterstack.com", {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${logtailSourceToken}`,
+      "Content-Type": "application/msgpack",
+    },
+    body: pack(logData),
+  });
 
-	const response = await next();
+  const response = await next();
 
-	return response;
+  return response;
 });
