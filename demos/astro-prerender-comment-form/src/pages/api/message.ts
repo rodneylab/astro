f573f95226ee72ba651feac09da6e7ca37cd4163
Rodney Lab <codesign@rodneylab.com>
2024-12-31 09:00:17 +0000

    build(demos/astro-prerender-comment-form): 🧱 update packages
    ┌─────────────────────┬─────────┬─────────┬──────────────────────────────┐
    │ Package             │ From    │ To      │ Dependents                   │
    ├─────────────────────┼─────────┼─────────┼──────────────────────────────┤
    │ stylelint           │ 16.10.0 │ 16.12.0 │ astro-prerender-comment-form │
    ├─────────────────────┼─────────┼─────────┼──────────────────────────────┤
    │ svelte              │ 5.7.1   │ 5.16.0  │ astro-prerender-comment-form │
    ├─────────────────────┼─────────┼─────────┼──────────────────────────────┤
    │ typescript (dev)    │ 5.6.3   │ 5.7.2   │ astro-prerender-comment-form │
    ├─────────────────────┼─────────┼─────────┼──────────────────────────────┤
    │ wrangler (dev)      │ 3.92.0  │ 3.99.0  │ astro-prerender-comment-form │
    ├─────────────────────┼─────────┼─────────┼──────────────────────────────┤
    │ @astrojs/cloudflare │ 11.2.0  │ 12.1.0  │ astro-prerender-comment-form │
    ├─────────────────────┼─────────┼─────────┼──────────────────────────────┤
    │ @astrojs/svelte     │ 5.7.3   │ 7.0.2   │ astro-prerender-comment-form │
    ├─────────────────────┼─────────┼─────────┼──────────────────────────────┤
    │ astro               │ 4.16.17 │ 5.1.1   │ astro-prerender-comment-form │
    ├─────────────────────┼─────────┼─────────┼──────────────────────────────┤
    │ vite (dev)          │ 5.4.11  │ 6.0.6   │ astro-prerender-comment-form │
    └─────────────────────┴─────────┴─────────┴──────────────────────────────┘

--- demos/astro-prerender-comment-form/src/pages/api/message.ts
+++ demos/astro-prerender-comment-form/src/pages/api/message.ts
@@ -1,35 +1,38 @@
-import type { APIRoute } from 'astro';
+import type { APIRoute } from "astro";
 
 const siteUrl = import.meta.env.PUBLIC_SITE_URL;
 const turnstileSecret = import.meta.env.TURNSTILE_SECRETKEY;
 
 export const POST: APIRoute = async function POST({ redirect, request }) {
-	try {
-		const form = await request.formData();
-		const name = form.get('name');
-		const email = form.get('email');
-		const comment = form.get('comment');
-		const turnstileResponse = form.get('cf-turnstile-response');
+  try {
+    const form = await request.formData();
+    const name = form.get("name");
+    const email = form.get("email");
+    const comment = form.get("comment");
+    const turnstileResponse = form.get("cf-turnstile-response");
 
-		const ip = request.headers.get('CF-Connecting-IP');
+    const ip = request.headers.get("CF-Connecting-IP");
 
-		if (typeof turnstileResponse === 'string') {
-			const bodyFormData = new FormData();
-			bodyFormData.append('secret', turnstileSecret);
-			bodyFormData.append('response', turnstileResponse);
-			ip && bodyFormData.append('remoteip', ip);
-			const response = await fetch('https://challenges.cloudflare.com/turnstile/v0/siteverify', {
-				method: 'POST',
-				body: bodyFormData,
-			});
-			const { success } = await response.json();
+    if (typeof turnstileResponse === "string") {
+      const bodyFormData = new FormData();
+      bodyFormData.append("secret", turnstileSecret);
+      bodyFormData.append("response", turnstileResponse);
+      ip && bodyFormData.append("remoteip", ip);
+      const response = await fetch(
+        "https://challenges.cloudflare.com/turnstile/v0/siteverify",
+        {
+          method: "POST",
+          body: bodyFormData,
+        },
+      );
+      const { success } = await response.json();
 
-			console.log({ name, email, comment, success });
-		}
+      console.log({ name, email, comment, success });
+    }
 
-		return redirect(`${siteUrl}/thanks`);
-	} catch (error: unknown) {
-		console.error(`Error in comment form submission: ${error as string}`);
-		return redirect(`${siteUrl}/`);
-	}
+    return redirect(`${siteUrl}/thanks`);
+  } catch (error: unknown) {
+    console.error(`Error in comment form submission: ${error as string}`);
+    return redirect(`${siteUrl}/`);
+  }
 };
