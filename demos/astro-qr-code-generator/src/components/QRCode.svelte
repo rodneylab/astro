fca82342030e56ec3e4b3d3ec40a4f86c2794484
Rodney Lab <codesign@rodneylab.com>
2025-01-01 07:43:52 +0000

    build(demos/astro-qr-code-generator): 🧱 update dependencies
    ┌──────────────────┬─────────┬─────────┬─────────────────────────┐
    │ Package          │ From    │ To      │ Dependents              │
    ├──────────────────┼─────────┼─────────┼─────────────────────────┤
    │ stylelint (dev)  │ 16.10.0 │ 16.12.0 │ astro-qr-code-generator │
    ├──────────────────┼─────────┼─────────┼─────────────────────────┤
    │ svelte           │ 5.7.1   │ 5.16.0  │ astro-qr-code-generator │
    ├──────────────────┼─────────┼─────────┼─────────────────────────┤
    │ typescript (dev) │ 5.6.3   │ 5.7.2   │ astro-qr-code-generator │
    ├──────────────────┼─────────┼─────────┼─────────────────────────┤
    │ @astrojs/netlify │ 5.5.4   │ 6.0.1   │ astro-qr-code-generator │
    ├──────────────────┼─────────┼─────────┼─────────────────────────┤
    │ @astrojs/svelte  │ 5.7.3   │ 7.0.2   │ astro-qr-code-generator │
    ├──────────────────┼─────────┼─────────┼─────────────────────────┤
    │ astro            │ 4.16.17 │ 5.1.1   │ astro-qr-code-generator │
    ├──────────────────┼─────────┼─────────┼─────────────────────────┤
    │ vite (dev)       │ 5.4.11  │ 6.0.6   │ astro-qr-code-generator │
    └──────────────────┴─────────┴─────────┴─────────────────────────┘

--- demos/astro-qr-code-generator/src/components/QRCode.svelte
+++ demos/astro-qr-code-generator/src/components/QRCode.svelte
@@ -1,32 +1,32 @@
 <script lang="ts">
-	import QRCode from 'qrcode';
+  import QRCode from "qrcode";
 
-	let { url }: { url: string } = $props();
+  let { url }: { url: string } = $props();
 
-	function generateQRCode(canvasElement: HTMLCanvasElement, url: string) {
-		QRCode.toCanvas(canvasElement, url, function (error: unknown) {
-			if (error) {
-				console.error(`Error creating QRCode: ${error as string}`);
-			}
-		});
-	}
+  function generateQRCode(canvasElement: HTMLCanvasElement, url: string) {
+    QRCode.toCanvas(canvasElement, url, function (error: unknown) {
+      if (error) {
+        console.error(`Error creating QRCode: ${error as string}`);
+      }
+    });
+  }
 
-	function useQRCodeCanvas(node: HTMLCanvasElement, { url }: { url: string }) {
-		generateQRCode(node, url);
+  function useQRCodeCanvas(node: HTMLCanvasElement, { url }: { url: string }) {
+    generateQRCode(node, url);
 
-		return {
-			update({ url: newUrl }) {
-				generateQRCode(node, newUrl);
-			},
-		};
-	}
+    return {
+      update({ url: newUrl }) {
+        generateQRCode(node, newUrl);
+      },
+    };
+  }
 </script>
 
 <canvas use:useQRCodeCanvas={{ url }}></canvas>
 
 <style>
-	canvas {
-		margin: 0 auto;
-		padding: auto;
-	}
+  canvas {
+    margin: 0 auto;
+    padding: auto;
+  }
 </style>
