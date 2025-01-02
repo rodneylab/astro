b28b4f69ab0643a21f628781587cba0a636fa1f6
Rodney Lab <codesign@rodneylab.com>
2025-01-02 11:34:45 +0000

    build(demos/astro-related-content): 🧱 update dependencies
    ┌─────────────────┬─────────┬─────────┬───────────────────────┐
    │ Package         │ From    │ To      │ Dependents            │
    ├─────────────────┼─────────┼─────────┼───────────────────────┤
    │ stylelint       │ 16.10.0 │ 16.12.0 │ astro-related-content │
    ├─────────────────┼─────────┼─────────┼───────────────────────┤
    │ svelte          │ 5.7.1   │ 5.16.0  │ astro-related-content │
    ├─────────────────┼─────────┼─────────┼───────────────────────┤
    │ typescript      │ 5.6.3   │ 5.7.2   │ astro-related-content │
    ├─────────────────┼─────────┼─────────┼───────────────────────┤
    │ @astrojs/svelte │ 6.0.2   │ 7.0.2   │ astro-related-content │
    ├─────────────────┼─────────┼─────────┼───────────────────────┤
    │ @iconify/svelte │ 3.1.6   │ 4.1.0   │ astro-related-content │
    ├─────────────────┼─────────┼─────────┼───────────────────────┤
    │ astro           │ 4.16.17 │ 5.1.1   │ astro-related-content │
    └─────────────────┴─────────┴─────────┴───────────────────────┘

--- demos/astro-related-content/src/components/RodneyLabCredit.svelte
+++ demos/astro-related-content/src/components/RodneyLabCredit.svelte
@@ -4,7 +4,12 @@
     href="https://rodneylab.com/"
     target="_blank"
     rel="noopener noreferrer"
-    ><img alt="Rodney Lab Logo" src="/assets/rodneylab-logo.png" height="16" width="16" /></a
+    ><img
+      alt="Rodney Lab Logo"
+      src="/assets/rodneylab-logo.png"
+      height="16"
+      width="16"
+    /></a
   >
   <a
     aria-label="Open the Rodney Lab site"
@@ -20,8 +25,10 @@
     font-family: Lato;
     font-style: normal;
     font-weight: var(--font-weight-light);
-    src: local(''), url('/fonts/lato-v22-latin-300.woff2') format('woff2')
-      /* Chrome 26+, Opera 23+, Firefox 39+ */
+    src:
+      local(""),
+      url("/fonts/lato-v22-latin-300.woff2") format("woff2");
+    /* Chrome 26+, Opera 23+, Firefox 39+ */
   }
 
   /* lato-regular - latin */
@@ -29,14 +36,16 @@
     font-family: Lato;
     font-style: normal;
     font-weight: var(--font-weight-normal);
-    src: local(''), url('/fonts/lato-v22-latin-regular.woff2') format('woff2')
-      /* Chrome 26+, Opera 23+, Firefox 39+ */ url('/fonts/lato-v22-latin-regular.woff')
-
+    src:
+      local(""),
+      url("/fonts/lato-v22-latin-regular.woff2") format("woff2")
+        /* Chrome 26+, Opera 23+, Firefox 39+ */
+        url("/fonts/lato-v22-latin-regular.woff");
   }
 
   .container {
-    display: 'flex';
-    align-items: 'center';
+    display: "flex";
+    align-items: "center";
     color: oklch(25.58% 0.054 239.31);
     font-family: Lato;
   }
